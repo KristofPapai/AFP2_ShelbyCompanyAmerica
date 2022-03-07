75cfa131e08c9b4c0a42ec30f458742ff6523b31
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-03-07 22:56:01 +0100

    MainController Első feltöltése
    MainController valamint a main.blade.php első feltöltése, ami majd a főoldal lesz. A web.php módosítása, hogy elérje a main.blade.php-t.

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/main', [\App\Http\Controllers\MainController::class, 'main']);
