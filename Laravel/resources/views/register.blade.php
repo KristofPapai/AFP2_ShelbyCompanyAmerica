8c17fba38e5bd5086a0490c6c27e4d50ca8ff792
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-03-13 13:37:09 +0100

    register page

---
+++ Laravel/resources/views/register.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>MyLearning Regisztráció</title>
+        <meta name="description" content="">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+        <style>
+            .body-bg {
+                background-color: #9921e8;
+                background-image: linear-gradient(315deg, #9921e8 0%, #5f72be 74%);
+            }
+        </style>
+    </head>
+    <body>
+    <header class="max-w-lg mx-auto">
+        <a href="#">
+            <h1 class="text-4xl font-bold text-white text-center">MyLearning</h1>
+            <h1 class="text-2xl text-white text-center">Knowledge is power</h1>
+        </a>
+    </header>
+
+    <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+        <section>
+            <h3 class="font-bold text-2xl">Regisztáció</h3>
+        </section>
+
+        <section class="mt-10">
+            <form class="flex flex-col" method="post" action='/checkregister'>
+                @csrf
+                <div class="mb-6 pt-3 rounded bg-gray-200">
+                    <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="name">Név</label>
+                    <input type="text" id="name" name="name" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" required value="">
+                </div>
+                <div class="mb-6 pt-3 rounded bg-gray-200">
+                    <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Jelszó</label>
+                    <input type="password" id="password" name="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" required value="">
+                </div>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password_again">Jelszó újra</label>
+                    <input type="password" id="password_again" name="password_again" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" required value="">
+                </div>
+                <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit" name="registration">Regisztáció</button>
+            </form>
+        </section>
+    </main>
+    </body>
+</html>
