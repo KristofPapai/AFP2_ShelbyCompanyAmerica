a1c06b89f9eefb7ee2e59cef6ee3dd3d32999713
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-03-13 16:02:27 +0100

    style correction

--- Laravel/resources/views/register.blade.php
+++ Laravel/resources/views/register.blade.php
@@ -39,10 +39,11 @@
                     <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Jelszó</label>
                     <input type="password" id="password" name="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" required value="">
                 </div>
-                <div>
+                <div class="mb-6 pt-3 rounded bg-gray-200">
                     <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password_again">Jelszó újra</label>
                     <input type="password" id="password_again" name="password_again" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" required value="">
                 </div>
+                <br/>
                 <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit" name="registration">Regisztáció</button>
             </form>
         </section>
