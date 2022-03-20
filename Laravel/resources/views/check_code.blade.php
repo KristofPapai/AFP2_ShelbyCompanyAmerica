3f27ced3e60cd4a009e37d51d1dc583c2b857255
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-03-20 11:49:58 +0100

    check_code nézet hozzáadása, send_to_email nézet frissítése

--- Laravel/resources/views/check_code.blade.php
+++ Laravel/resources/views/check_code.blade.php
@@ -28,17 +28,17 @@
 <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
     <section>
         <h3 class="font-bold text-2xl">Elfelejtett jelszó</h3>
-        <p>Kérjük adja meg az e-mail címét az ellenőrző kódhoz</p>
+        <p>Kérjük adja meg az emailben elküldött kódot</p>
     </section>
 
     <section class="mt-10">
-        <form class="flex flex-col" method="post" action='/send_email'>
+        <form class="flex flex-col" method="post" action='/checkcode'>
             @csrf
             <div class="mb-6 pt-3 rounded bg-gray-200">
-                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Email</label>
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="code">Kód:</label>
                 <input type="email" id="email" name="email" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" required value="">
             </div>
-            <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit" name="submit">Küldés</button>
+            <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit" name="submit">Ok</button>
         </form>
     </section>
 </main>
