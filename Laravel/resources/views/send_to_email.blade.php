7299a982b5ae980f9520e0bccc92fed04bb3f026
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-03-29 11:09:57 +0200

    Verifikációs kód elküldése E-mailben

--- Laravel/resources/views/send_to_email.blade.php
+++ Laravel/resources/views/send_to_email.blade.php
@@ -34,6 +34,7 @@
     <section class="mt-10">
         <form class="flex flex-col" method="post" action='/send_email'>
             @csrf
+            <h1>{{$code}}</h1>
             <div class="mb-6 pt-3 rounded bg-gray-200">
                 <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="neptun">Neptun kód</label>
                 <input type="text" id="neptun" name="neptun" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" required value="">
