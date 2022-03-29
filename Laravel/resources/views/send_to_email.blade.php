1e66d006948d780673655ffbcddd95c70f4bc6ce
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2022-03-29 21:10:23 +0200

    password resend redesign
    background gradient redesign

--- Laravel/resources/views/send_to_email.blade.php
+++ Laravel/resources/views/send_to_email.blade.php
@@ -11,13 +11,30 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
     <style>
-        .body-bg {
-            background-color: #9921e8;
-            background-image: linear-gradient(315deg, #9921e8 0%, #5f72be 74%);
+        body {
+            font-family: "Inter", sans-serif;
+        }
+
+        .background-animate {
+            background-size: 400%;
+
+            -webkit-animation: AnimationName 6s ease infinite;
+            -moz-animation: AnimationName 6s ease infinite;
+            animation: AnimationName 6s ease infinite;
+        }
+
+        @keyframes AnimationName {
+            0%,
+            100% {
+            background-position: 0% 50%;
+            }
+            50% {
+            background-position: 100% 50%;
+            }
         }
     </style>
 </head>
-<body class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+<body class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0 bg-gradient-to-r from-indigo-500 via-red-500 to-yellow-500 background-animate" style="font-family:'Lato',sans-serif;">
 <header class="max-w-lg mx-auto">
     <a href="#">
         <h1 class="text-4xl font-bold text-white text-center">MyLearning</h1>
