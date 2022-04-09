218242d2abe984be1738e75a2974fe7f22b061ef
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-04-09 18:33:04 +0200

    checkpassword funkció implementálása

--- Laravel/resources/views/options.blade.php
+++ Laravel/resources/views/options.blade.php
@@ -78,7 +78,7 @@
         <div>
             <form show method="post" action='/checkpassword' class="flex flex-col">
                 @csrf
-                Neptun-Kód <input type="text" name="neptun" required value={{$neptun}} class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"><br></br>
+                Neptun-Kód <input type="text" name="neptun" readonly required value={{$neptun}} class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"><br></br>
                 Név: <input type="text" name="name" required value={{$name}} class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"><br></br>
                 Régi jelszó: <input type="password"  name="old_password" required value="" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"><br></br>
                 Új jelszó: <input type="password" name="new_password" required value=""class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3" ><br></br>
