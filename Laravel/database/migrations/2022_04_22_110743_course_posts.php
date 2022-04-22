b7a8fb3c614c6cbf028d8752ed9218af0bffd0ae
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-22 13:29:20 +0200

    Kurzus tananyag tábla létrhozása

---
+++ Laravel/database/migrations/2022_04_22_110743_course_posts.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_posts', function (Blueprint $table) {
+            $table->increments('post_id');
+            $table->string('course_id',4);
+            $table->string('post_name',100);
+            $table->string('post',10000);
+            $table->timestamps();
+            $table->foreign('course_id')->references('course_id')->on('courses');
+        });
+        DB::table('course_posts')->insert([
+            'post_id' => '1',
+            'course_id' => 'AAAA',
+            'post_name'=> 'AAAAAAA',
+            'post' => 'AAAAAAAAAAAAAAAAAAAAAAAAAA'
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_posts');
+    }
+};
