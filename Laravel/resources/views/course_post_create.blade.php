9be476230b2bfb4a8a6af65e3af1a497294ba390
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-04-22 15:50:36 +0200

    Tananyag létrehozás nézet frissítése

--- Laravel/resources/views/course_post_create.blade.php
+++ Laravel/resources/views/course_post_create.blade.php
@@ -1,7 +1,8 @@
 @extends('course_master')
 @section('title', 'Tananyag készítés')
 @section('content')
-    <form >
+    <form method="POST" action="{{route('createcoursepost', $course_id)}}">
+        @csrf
         <input value="Tananyag Cím" name="post_title">
         <input value="Tananyag Tartalom" name="post_content">
         <input type="submit" value="Kész">
